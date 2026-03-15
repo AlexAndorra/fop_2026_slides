bb2cf53788405fa1c7112859fb23df5eb17b3b50
AlexAndorra <AlexAndorra@users.noreply.github.com>
2026-03-15 22:56:56 +0000

    deploy: 7d4110d360a68db9c91fb6f6a0e72c5d2229823b

--- assets/slidev/print-DyYnn9u6.js
+++ assets/slidev/print-DyYnn9u6.js
@@ -1,4 +1,4 @@
-import{f as m,$ as f,y as u,o as i,e as r,g as e,t as a,F as h,Z as g,a0 as v,d as x,j as b}from"../modules/vue-C6IPDUIV.js";import{u as N,j as y,c as d,b as k}from"../index-DpPh6jcd.js";import{N as w}from"./NoteDisplay-BXP8IKZm.js";import"../modules/shiki-abW-5elt.js";const S=m({__name:"print",setup(p,{expose:n}){n();const{slides:l,total:o}=N();f(`
+import{f as m,$ as f,y as u,o as i,e as r,g as e,t as a,F as h,Z as g,a0 as v,d as x,j as b}from"../modules/vue-C6IPDUIV.js";import{u as N,j as y,c as d,b as k}from"../index-Qhx1uJq5.js";import{N as w}from"./NoteDisplay-Ddz3FWet.js";import"../modules/shiki-abW-5elt.js";const S=m({__name:"print",setup(p,{expose:n}){n();const{slides:l,total:o}=N();f(`
 @page {
   size: A4;
   margin-top: 1.5cm;
